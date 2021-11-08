fb1de0eba16d22d70ec81aa51d34a953584d68f9
sjb0323 <sjb0323@gmail.com>
2021-11-09 00:07:44 +0900

    211109

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-import dash
+from dash import dash, html, Input, Output, callback_context
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -20,9 +20,11 @@ app.layout = html.Div([
 
     dbc.Row([
         dbc.Col([
-            dbc.Row(dbc.Col(html.Div("chart1"), width = 12, style = {"background":"red"})),
-            dbc.Row(dbc.Col(html.Div("chart2"), width = 12, style = {"background":"pink"})),
-        ]),
+            dbc.Row(dbc.Col(html.Div(["chart1",
+            dcc.Graph(id='main-graph')]),width = 12, style = {"background":"red"})),
+            dbc.Row(dbc.Col(html.Div(["chart2",
+            dcc.Graph(id='second-graph')]),width = 12, style = {"background":"pink"})),
+        ], width = 9),
 
         dbc.Col([
             html.H1("Options"),
@@ -35,11 +37,31 @@ app.layout = html.Div([
             
             html.H3("Selected User Traits"),
             html.H5("Gender"),
-            html.Div(
-                dbc.ButtonGroup(
-                    [dbc.Button("Male"), dbc.Button("Female")]
-                ),
+            html.Div([
+                    dbc.RadioItems(
+                        id="radios",
+                        className="btn-group",
+                        inputClassName="btn-check",
+                        labelClassName="btn btn-outline-primary",
+                        labelCheckedClassName="active",
+                        options=[
+                            {"label": "Male", "value": 1},
+                            {"label": "Female", "value": 2},
+                        ],
+                        value=1,
+                    ),
+                    #html.Div(id="output"),
+                ],
+                className="radio-group",
             ),
+
+            
+            # html.Div(
+            #     dbc.ButtonGroup(
+            #         [dbc.Button("Male", id = "Male-Button"), dbc.Button("Female")]
+            #     ),
+            # ),
+
             html.H5("Neuroticism"),
             html.Div(
                 dbc.ButtonGroup(
@@ -71,14 +93,29 @@ app.layout = html.Div([
     ], className ="g-2"),
 
     dbc.Row([
-        dbc.Col(html.Div("chart3"), width = 3, style = {"background":"red"}),
-        dbc.Col(html.Div("chart4"), width = 6, style = {"background":"red"}),
-        dbc.Col(html.Div("chart5"), width = 3, style = {"background":"red"}),
+        dbc.Col(html.Div(["chart3",
+        dcc.Graph(id='third-graph')]), width = 3, style = {"background":"red"}),
+        dbc.Col(html.Div(["chart4",
+        dcc.Graph(id='fourth-graph')]), width = 6, style = {"background":"red"}),
+        dbc.Col(html.Div(["chart5",
+        dcc.Graph(id='fifth-graph')]), width = 3, style = {"background":"red"}),
     ])
-
 ])
- 
 
+
+@app.callback(Output("output", "children"), [Input("radios", "value")])
+def display_value(value):
+    return f"Selected value: {value}"
+
+#@app.callback(
+    #Output(component_id="Male_button", "children"),
+    #Input("component_id="Male_button")
+#)
+# def on_button_click(n):
+#     if n is None:
+#         return "Not clicked."
+#     else:
+#         return "Clicked {n} times."
 
     
 
