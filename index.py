6817dd65c7ab380251a19125976b3d25541ad03b
Juhyeong Park <wngud913@gmail.com>
2021-11-10 15:59:14 +0900

    commint on main

--- index.py
+++ index.py
@@ -1,15 +1,44 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-from dash import dash, html, Input, Output, callback_context
+import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
+import time
+import pandas as pd
+import datetime as dt 
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.graph_objects import Layout
+
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server,external_stylesheets=[dbc.themes.FLATLY])
 
+daterange = pd.date_range(start=dt.datetime(2000, 1, 1)+ dt.timedelta(hours=9),end=dt.datetime(2000, 1, 2)+ dt.timedelta(hours=9),freq='S')
 
+def unixTimeMillis(dt):
+    ''' Convert datetime to unix timestamp '''
+    return int(time.mktime(dt.timetuple()))
 
+def unixToDatetime(unix):
+    ''' Convert unix timestamp to datetime. '''
+    return pd.to_datetime([unix],unit='s').time[0]
+
+def getMarks(start, end, Nth=100):
+    ''' Returns the marks for labeling. 
+        Every Nth value will be used.
+    '''
+
+    result = {}
+    for i, date in enumerate(daterange):
+        if(i%Nth == 1):
+            # Append value to dict
+            result[unixTimeMillis(date)] = str(date.strftime('%Y-%m-%d'))
+    return result
+
+df = pd.read_csv('data/concat.csv')
+userInfo_df = pd.read_csv('data/user_info.csv')
 
 app.layout = html.Div([
     dbc.Row(
@@ -99,11 +128,45 @@ app.layout = html.Div([
         dcc.Graph(id='fourth-graph')]), width = 6, style = {"background":"red"}),
         dbc.Col(html.Div(["chart5",
         dcc.Graph(id='fifth-graph')]), width = 3, style = {"background":"red"}),
-    ])
+    ]),
+    
+    html.Div([
+    dcc.Graph(
+        id="scatter-plot",
+        hoverData={'points': [{'hovertext': 'P0701'}]}
+        ), 
+    ],style={'width': '500px'},
+    ),
+    html.Div(
+        [
+            html.Label('From 00:00:00 to 23:59:59', id='time-range-label'),
+            dcc.RangeSlider(
+                id='time_slider',
+                min = unixTimeMillis(daterange.min()),
+                max = unixTimeMillis(daterange.max()),
+                value = [unixTimeMillis(daterange.min()),
+                         unixTimeMillis(daterange.max())],
+            ),
+        ],
+        style={'margin-top': '20', 'width': '500px'}
+    ),
+    html.Div(
+        [
+            html.Label('USER ID', id='user-id-label'),
+        ],
+        style={'margin-top': '20', 'width': '500px'}
+    ),
+    html.Hr(),
+    html.Div([
+    dcc.Graph(
+        id="polar-plot",
+        ), 
+    ],style={'width': '500px'},
+    )
 ])
 
 
-@app.callback(Output("output", "children"), [Input("radios", "value")])
+@app.callback(dash.dependencies.Output("output", "children"), [dash.dependencies.Input("radios", "value")])
 def display_value(value):
     return f"Selected value: {value}"
 
@@ -116,6 +179,85 @@ def display_value(value):
 #         return "Not clicked."
 #     else:
 #         return "Clicked {n} times."
+@app.callback(
+    dash.dependencies.Output('polar-plot', 'figure'),
+    [dash.dependencies.Input('scatter-plot', 'hoverData')])
+def update_user_Id(hoverData):
+    userId=hoverData['points'][0]['hovertext']
+    userNumb=int(userId[1:])
+
+    polar_df=pd.DataFrame()
+
+    polar_df['theta']= ['Openness','Conscientiousness','Neuroticism','Extraversion','Agreeableness']
+
+    data_df=userInfo_df.loc[userInfo_df['UID']==userNumb]
+    data=[]
+
+    for i in range(1,6,1):
+        data.append(data_df.iloc[0][i])
+
+    polar_df['data']=data
+
+    fig=px.line_polar(polar_df, r="data", theta="theta", range_r=[0,15], line_close=True)
+    
+    
+    return fig
+
+
+@app.callback(
+    dash.dependencies.Output('user-id-label', 'children'),
+    [dash.dependencies.Input('scatter-plot', 'hoverData')])
+def update_user_Id(hoverData):
+    userId=hoverData['points'][0]['hovertext']
+    return 'user id : {}'.format(userId)
+    
+@app.callback(
+    dash.dependencies.Output('time-range-label', 'children'),
+    [dash.dependencies.Input('time_slider', 'value')])
+def _update_time_range_label(year_range):
+    low, high = year_range
+    low=low%86400+32400
+    high=(high-1)%86400+32400
+
+    return 'From {} to {}. {}~{}'.format(unixToDatetime(year_range[0]),
+                                  unixToDatetime(year_range[1]),low,high)
+
+@app.callback(
+    dash.dependencies.Output("scatter-plot", "figure"), 
+    [dash.dependencies.Input("time_slider", "value")])
+def update_chart(slider_range):
+    low, high = slider_range
+    low=low%86400
+    high=(high-1)%86400
+    mask = (df['timestamp'] > low) & (df['timestamp'] < high)
+    scatter_df=df[mask]
+
+    layout = Layout(plot_bgcolor='rgba(0,0,0,0)')
+
+    fig = go.Figure(layout = layout)
+    unique_y=scatter_df['name'].unique()
+    fig=px.scatter(
+    scatter_df,
+    x=scatter_df.convertTime,          
+    y=scatter_df.name,
+    hover_name=scatter_df.ID
+    )
+    fig.update_traces(
+        marker = dict(
+        size = 7, 
+        color = 'black', 
+        symbol='line-ns',
+        opacity = 0.2,
+        line = dict(
+            color='red',
+            width=2
+        )
+    )
+    )
+    fig.update_layout(plot_bgcolor = 'rgba(0,0,0,0)')
+    fig.update_yaxes(showline=True, gridwidth=7, gridcolor="#eee")
+
+    return fig
 
     
 
